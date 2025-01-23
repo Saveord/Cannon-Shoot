cad47557dca163c2d2495bbd99ac8162eaf1d337
Alexa Wu <porkson12@gmail.com>
2025-01-22 21:36:45 -0800

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cannon c = new Cannon();
+
+        float t = 0;
+        float dt = 0.2f;
+
+        for (int n = 0 ; n < 10 ; n++) {
+            t = n * dt;
+            Double[] p = c.position(3.14f / 3f, 20f, t);
+            System.out.println(t + ": (" + p[0] + ", " + p[1] + ")");
+        }
+    }
+}
